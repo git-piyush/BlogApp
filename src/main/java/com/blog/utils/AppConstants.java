6f263e2041ca6dfe651a50dde33801eb1deb87b8
Piyush kumar <kmrpiyush95@gmail.com.com>
2022-06-04 21:38:04 +0530

    Creating AppConstants.java to configure constants values

---
+++ src/main/java/com/blog/utils/AppConstants.java
@@ -0,0 +1,11 @@
+package com.blog.utils;
+
+public class AppConstants {
+
+	public static final String DEFAULT_PAGE_NUMBER = "0";
+	public static final String DEFAULT_PAGE_SIZE = "5";
+	public static final String DEFAULT_SORTBY = "id";
+	public static final String DEFAULT_ORDERBY = "ASC";
+	
+	
+}
